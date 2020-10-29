0d1294e0ea8003d01a2d00c6c24a927babd09263
Christian Gerace <cgerace8@gmail.com>
2020-10-29 17:53:08 -0400

    Added Signup Resolver

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,28 @@
+const bcrypt = require('bcryptjs');
+
 const { users, tasks } = require('../constants');
+const User = require('../database/models/user');
 
 module.exports = {
     Query: {
         users: () => users,
         user: (_, { id }) => users.find(user => user.id === id)
+    },
+    Mutation: {
+        signup: async (_, { input }) => {
+            try {
+                const user = await User.findOne({ email: input.email })
+                if (user) {
+                    throw new Error('Email already in use');
+                }
+                const hashedPassword = await bcrypt.hash(input.password, 12);
+                const newUser = new User({...input, password: hashedPassword });
+                const result = await newUser.save();
+                return result;
+            } catch (error) {
+                throw error;
+            }
+        }
     },
     User: {
         tasks: (parent) => tasks.filter(task => task.userId === parent.id)
